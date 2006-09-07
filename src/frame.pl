2eff0a0f2427c9527f8bb720f56e842e919ea3f1
Johan Vromans <jvromans@squirrel.nl>
2006-09-07 16:19:26 +0000

    Tool to make a smaller, enframed copy of an image (for the gallery).

---
+++ src/frame.pl
@@ -0,0 +1,240 @@
+#!/usr/bin/perl -w
+my $RCS_Id = '$Id$ ';
+
+# Skeleton for Getopt::Long.
+
+# Author          : Johan Vromans
+# Created On      : Tue Sep 15 15:59:04 1992
+# Last Modified By: Johan Vromans
+# Last Modified On: Fri Aug 26 15:55:05 2005
+# Update Count    : 143
+# Status          : Unknown, Use with caution!
+
+################ Common stuff ################
+
+use strict;
+
+# Package or program libraries, if appropriate.
+# $LIBDIR = $ENV{'LIBDIR'} || '/usr/local/lib/sample';
+# use lib qw($LIBDIR);
+# require 'common.pl';
+
+# Package name.
+my $my_package = 'Sciurix';
+# Program name and version.
+my ($my_name, $my_version) = $RCS_Id =~ /: (.+).pl,v ([\d.]+)/;
+# Tack '*' if it is not checked in into RCS.
+$my_version .= '*' if length('$Locker$ ') > 12;
+
+################ Command line parameters ################
+
+use Getopt::Long 2.13;
+
+# Command line options.
+my $theyear = 0;
+my $thisyear = 1900 + (localtime(time))[5];
+my $cprcolor = "grey";
+my $verbose = 0;		# verbose processing
+
+# Development options (not shown with -help).
+my $debug = 0;			# debugging
+my $trace = 0;			# trace (show process)
+my $test = 0;			# test mode.
+
+# Process command line options.
+app_options();
+
+# Post-processing.
+$trace |= ($debug || $test);
+
+################ Presets ################
+
+my $TMPDIR = $ENV{TMPDIR} || $ENV{TEMP} || '/usr/tmp';
+
+################ The Process ################
+
+use File::Basename;
+use Image::Magick;
+
+my $year;
+
+my $cpr = "Copyright YEAR Johan Vromans. All rights reserved.\n".
+  "Any form of reproduction is prohibited without prior written permission\n".
+  "of the copyright holder.\n".
+  "http://johan.vromans.org";
+
+my $olay; # = new Image("/home/jv/wrk/cpr-black.xcf");
+
+foreach my $file ( @ARGV ) {
+    $file =~ s;\\;/;g;
+    my $dir = dirname($file);
+    my $med = $dir;
+    my $sml = $dir;
+    my $base = basename($file);
+    $base =~ s/\.[^.]+$//;
+
+    if ( $dir =~ m;^(.*/)?large$; ) {
+	$med = ($1||"") . "medium/$base.jpg";
+	$sml = ($1||"") . "thumbnails/$base.jpg";
+    }
+    else {
+	$med = $dir . "/" . $base . "-med.jpg";
+	$sml = $dir . "/" . $base . "-sml.jpg";
+    }
+
+    if ( $theyear ) {
+	$year = $theyear;
+    }
+    elsif ( $base =~/^(\d\d\d\d)/ && $1 > 1900 && $1 < 2099 ) {
+	$year = $1;
+    }
+    else {
+	$year = $thisyear;
+    }
+    $cpr =~ s/YEAR/$year/g;
+
+    my $im = new Image($file);
+    warn ("$file -> $med\n") if $verbose;
+    $im->resize(500)->clone->overlay($olay)->frame->write($med);
+    warn ("$file -> $sml\n") if $verbose;
+    $im->resize(138)->frame(6)->write($sml);
+}
+
+exit 0;
+
+################ Subroutines ################
+
+package Image;
+
+sub new {
+    my ($pkg, $file) = (@_);
+    my $image = new Image::Magick;
+    my $t = $image->Read($file);
+    warn("read($file): $t\n") if $t;
+    $image->Profile(name => "*", profile => '');
+    $image->Comment($cpr);
+    bless \$image;
+}
+
+sub resize {
+    my ($self, $n) = @_;
+    # height may not exceed n.
+    # width may not exceed 4/3 x n.
+
+    my ($origx, $origy) = $$self->Get(qw(width height));
+    my $ratioy = $origy / $n;
+    my $ratiox = $origx / ((4 * $n) / 3);
+    my $ratio = $ratiox > $ratioy ? $ratiox : $ratioy;
+    warn("resize: width => ", $origx/$ratio, " height => ", $origy/$ratio, "\n")
+      if $trace;
+    my $t = $$self->Resize(width => $origx/$ratio, height => $origy/$ratio);
+    warn("resize: $t\n") if $t;
+    $self;
+}
+
+sub xxresize {
+    my ($self, $n) = @_;
+    # max(width, height) may not exceed n.
+    my ($origx, $origy) = $$self->Get(qw(width height));
+    my $ratio = $origx > $origy ? $origx / $n : $origy / $n;
+    warn("resize: width => ", $origx/$ratio, " height => ", $origy/$ratio, "\n")
+      if $trace;
+    my $t = $$self->Resize(width => $origx/$ratio, height => $origy/$ratio);
+    warn("resize: $t\n") if $t;
+    $self;
+}
+
+sub clone {
+    my ($self) = @_;
+    my $new = $$self->Clone();
+    bless \$new;
+}
+
+sub frame {
+    my ($self, $sz) = @_;
+    $sz ||= 10;
+    my $is = int($sz/2);
+    warn("frame: width => $sz, height => $sz, inner => $is, outer => $is\n")
+      if $trace;
+    $$self->Frame(width => $sz, height => $sz, inner => $is, outer => $is);
+    $self;
+}
+
+sub overlay {
+    my ($self, $img) = @_;
+    my ($origx, $origy) = $$self->Get(qw(width height));
+    my $color = lc($cprcolor);
+    my $align = "right";
+    my @aa = (text => " \xa9 $year Johan Vromans ",
+	      font => "Helvetica",
+	      pointsize => 14,
+	      antialias => 'true',
+	      'y' => $origy - 4,
+	      encoding => 'ISO-8859-1');
+    if ( $color =~ /^([lr]):(.*)$/ ) {
+	if ( $1 eq 'l' ) {
+	    push(@aa, x => 0, align => 'left');
+	}
+	else {
+	    push(@aa, x => $origx, align => 'right');
+	}
+	$color = $2;
+    }
+    else {
+	push(@aa, x => $origx, align => 'right');
+    }
+    push(@aa, stroke => $color);
+    warn("annotate: @aa\n") if $trace;
+    my $t = $$self->Annotate(@aa);
+    warn("annotate: $t\n") if $t;
+    $self;
+}
+sub write {
+    my ($self, $file) = @_;
+    my $t = $$self->Write($file);
+    warn("write: $t\n") if $t;
+    $self;
+}
+
+package main;
+
+################ Subroutines ################
+
+sub app_options {
+    my $help = 0;		# handled locally
+    my $ident = 0;		# handled locally
+
+    # Process options, if any.
+    # Make sure defaults are set before returning!
+    return unless @ARGV > 0;
+
+    if ( !GetOptions(
+		     'year=i'   => \$theyear,
+		     'anncolor=s' => \$cprcolor,
+		     'ident'	=> \$ident,
+		     'verbose'	=> \$verbose,
+		     'trace'	=> \$trace,
+		     'help|?'	=> \$help,
+		     'debug'	=> \$debug,
+		    ) or $help )
+    {
+	app_usage(2);
+    }
+    app_ident() if $ident;
+}
+
+sub app_ident {
+    print STDERR ("This is $my_package [$my_name $my_version]\n");
+}
+
+sub app_usage {
+    my ($exit) = @_;
+    app_ident();
+    print STDERR <<EndOfUsage;
+Usage: $0 [options] [file ...]
+    -help		this message
+    -ident		show identification
+    -verbose		verbose information
+EndOfUsage
+    exit $exit if defined $exit && $exit != 0;
+}
