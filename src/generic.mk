5e26c52b085b3df0b4d4830a460ba67f35da449b
Johan Vromans <jvromans@squirrel.nl>
2026-06-15 22:10:27 +0200

    Re-add fetch target in generic.mk.

--- src/generic.mk
+++ src/generic.mk
@@ -1,71 +1,50 @@
 # Generic Makefile for albums.
 
-HERE	= .
-TOOLS	= $(HOME)/src/album/src
-DATA	= $(HOME)/src/album/data
-#CAMERA	= /mnt/camera
-CAMERA	?= /mnt/mstick
-DCIM	?= dcim/101msdcf
-DSC	?= $(CAMERA)/$(DCIM)
-DSCMP	?= $(CAMERA)/mp_root/101anv01
-RAW	?= $(HERE)/$(DCIM)
-PFX	?= dsc0
-OPTS	?=
-PERL	?= perl
-WCAPTION ?= tc
-# UPLOAD  = warpnet:vromans.org/johan/albums/20080831/
-UPLOAD  ?= /tmp/
+BASE    = $(notdir ${PWD})
 
-IMPORT	?= $(shell test -d $(DCIM) && echo "--dcim=$(DCIM)")
+TOOLS	 = $(HOME)/src/album/src
+DATA	 = $(HOME)/src/album/data
 
-default :: update
+_DCIM    := dcim
+_DCF     := 101msdcf
+DCIM	 ?= ${_DCIM}/${_DCF}
+RAW	 ?= ${DCIM}
 
-fetch ::	mountc _fetch umountc
+OPTS	 ?=
+PERL	 ?= perl
+ALBUMCMD := $(PERL) -w $(TOOLS)/album.pl $(OPTS) --verbose
 
-mountc :
-	-mount $(CAMERA)
+ifdef TARGET
+default upload :: to-${TARGET}
+else
+default :: update update-ll
+endif
 
-_fetch : _fetch_dsc _fetch_mp _fix_prot
+################ ALBUM ################
 
-_fetch_dsc:
-	rsync -rtlDv --modify-window=1 --exclude=${PFX}0000.jpg \
-	    --itemize-changes \
-	    $(DSC)/ $(RAW)/
-
-_fetch_mp:
-	rsync -rtlDv --modify-window=1 --exclude=*.thm \
-	    --itemize-changes \
-	    $(DSCMP)/ $(RAW)/
-
-_fix_prot:
-	find $(RAW) -type f -perm /333 -print -exec chmod 0444 {} \;
-
-umountc :
-	-umount $(CAMERA)
+IMPORT ?= $(shell test -d $(RAW) && echo "--dcim=$(RAW)")
 
 update ::
-	$(PERL) -w $(TOOLS)/album.pl $(OPTS) --verbose --update $(IMPORT) $(HERE)
+	${ALBUMCMD} --update $(IMPORT)
+
+update-ll ::
+	perl ${TOOLS}/updatellink.pl
 
 clobber ::
-	$(PERL) -w $(TOOLS)/album.pl $(OPTS) --verbose --clobber --update $(IMPORT) $(HERE)
+	${ALBUMCMD} --clobber --update $(IMPORT)
 
-export-web ::
-	$(PERL) -w $(TOOLS)/album.pl $(OPTS) --verbose --mediumonly --caption=$(WCAPTION) $(HERE)
-	rm -f web.zip
-	zip -rn .jpg web.zip index*.html icons css medium index journal
+merge ::
+	$(PERL) $(TOOLS)/merge.pl info.dat > x~
+	if cmp info.dat x~ >/dev/null; then \
+	  rm -f x~; \
+	else \
+	  cp -p info.dat info.dat~ && cp -p x~ info.dat && rm x~; \
+	fi
 
-upload ::
-	$(PERL) -w $(TOOLS)/album.pl $(OPTS) --verbose --mediumonly --caption=$(WCAPTION) $(HERE)
-	rsync -avH --delete --delete-excluded \
-	  --exclude Makefile --exclude info.dat --exclude shellrun.exe \
-	  --exclude autorun.inf --exclude '.cache' --include '.htaccess' --exclude '*~' \
-	  --exclude web.zip --exclude dcim --exclude large \
-	  ./ $(UPLOAD)/
+################ SETUP ################
 
 init ::
 	mkdir -p $(DCIM)
-	: ln -s $(TOOLS)/shellrun.exe .
-	: ln -s $(TOOLS)/autorun.inf .
 	test -f Makefile || echo "include $(TOOLS)/generic.mk" > Makefile
 	test -f info.dat || { \
 		dir="`basename \`pwd\``"; \
@@ -73,24 +52,6 @@ init ::
 		ln -s $(DATA)/$$dir.dat info.dat; \
 		echo "!title $$dir" > info.dat; \
 	}
-
-init-nolinks ::
-	mkdir -p $(DCIM)
-	cp $(TOOLS)/shellrun.exe .
-	cp $(TOOLS)/autorun.inf .
-	test -f Makefile || cp $(TOOLS)/generic.mk Makefile
-	test -f info.dat || { \
-		dir="`basename \`pwd\``"; \
-		echo "!title $$dir" > info.dat; \
-	}
-
-gimp ::
-	@echo -n "Number? "; read a; \
-	cd large; file=`ls *$$a.jpg`; \
-	echo $$file; \
-	cat $$file > xx; rm -f $$file; mv xx $$file; \
-	gimp $$file; \
-	cd ..; rm -f medium/$$file index/$$file .cache
 
 clean ::
 	rm -f .cache *png index*html large/*html *~
@@ -100,13 +61,52 @@ clean ::
 realclean :: clean
 	rm -f `readlink info.dat` info.dat 
 
-to-notty ::
-	perl -ne 'END{print"- *\n"} print "+ /$$1\n" if /^(\S+\.jpg)/ && !/\s+--\s/' info.dat > .filter
-	rsync -rlptDvH --modify-window=3601 --filter=". .filter" large/ notty:${NOTTY_DEST}/
-
-P2BASE := $(shell basename `pwd`)
+################ PAR2 ################
 
 par2 ::
 	cd dcim; \
 	rm -fr .xvpics; \
-	par2 c -R ${P2BASE} 101msdcf
+	par2 c -R ${BASE} 101msdcf
+
+################ FETCH ################
+
+WIFIDEV := tz200
+POWEROFF :=
+
+fetch ::	fetch_${WIFIDEV}
+
+fetch_tz200 ::
+	perl ${HOME}/src/Lumix-Tools/scripts/fetch.pl \
+	     --path=${RAW} --exclude='????0001.jpg' ${POWEROFF}; \
+
+################ EXPORT ################
+
+WCAPTION ?= tc
+
+export-web ::
+	${ALBUMCMD} --mediumonly --caption=$(WCAPTION)
+	rm -f web.zip
+	zip -rn .jpg web.zip index*.html icons css medium index journal
+
+################ UPLOADING ################
+
+RESINFO := resinfo
+
+# Rsync filter to copy all images, omitting -- and movies.
+.filter : info.dat
+	perl -ne 'END{print"- *\n"} print "+ /$$1\n" if /^(\S+\.(?:jpg|png|webp))/ && !/\s+--\s/' $< > $@
+
+define define_uploader
+to-$(1) to_$(1) : .filter ;
+	rsync -virltD --modify-window=3601 \
+	  --delete --delete-excluded --filter=". .filter" \
+	  large/ \
+	  $(1):$(shell ${RESINFO} albums.$(1).path)/${BASE}/
+endef
+
+ifdef TARGET
+$(call define_uploader,${TARGET})
+else
+TARGETS := glaxxy glaxs4 glaxs7 glaxa8 largo
+$(foreach host,${TARGETS},$(call define_uploader,$(host)))
+endif
